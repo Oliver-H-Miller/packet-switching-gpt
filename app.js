307dc62335fe9188bd7356bfed0804ad0d8138c8
Oliver Miller <miller.oliver@icloud.com>
2023-12-03 21:36:42 -0500

    initial dirty version prototype

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const app = express()
 const path = require('path')
 const PORT = 4000
 
-
-
 app.use('/static', express.static(path.join(__dirname, 'static')))
 
 app.get('/', (req, res) => {
@@ -14,17 +12,26 @@ app.get('/', (req, res) => {
 })
 
 app.get('/gpt', async (req, res) => {
-    const OPEN_AI_API_KEY = req.query.key;
-    // validate key
-    if (!OPEN_AI_API_KEY || (OPEN_AI_API_KEY.length > 55 || OPEN_AI_API_KEY.length < 45)) {
-        res.status(400).json('Invalid API Key');
-        return;
-    }
+    /* DO NOT PUBLISH */
+    const OPEN_AI_API_KEY = "REDACTED";
+    // const OPEN_AI_API_KEY = req.query.key;
+    // // validate key
+    // if (!OPEN_AI_API_KEY || (OPEN_AI_API_KEY.length > 55 || OPEN_AI_API_KEY.length < 45)) {
+    //     res.status(400).json('Invalid API Key');
+    //     return;
+    // }
     // create gpt instance
     const gpt = new OpenAI({apiKey: OPEN_AI_API_KEY});
     const chatCompletion = await gpt.chat.completions.create({
-        messages: [{ role: 'user', content: 'Tell me a joke' }],
-        model: 'gpt-3.5-turbo',
+        messages: [
+            {
+              role: "system",
+              content: "You are a helpful assistant designed to output JSON.",
+            },
+            { role: "user", content: "Who won the world series in 2020?" },
+          ],
+          model: "gpt-3.5-turbo-1106",
+          response_format: { type: "json_object" },      
     });
     res.status(200).json(chatCompletion);
 })
